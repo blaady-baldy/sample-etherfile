5b343a93118677ebac5bc4e171bc385c1f1b2fe6
Devansh Singh <56143122+blaady-baldy@users.noreply.github.com>
2022-07-17 16:13:24 +0530

    Update brand.sol

--- brand.sol
+++ brand.sol
@@ -3,9 +3,16 @@
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/KeeperCompatibleInterface.sol";
+// import "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol";
 
 
-contract brand is ERC721, Ownable{
+error NFT_decayed();
+error Admin__UpkeepNotTrue();
+error Admin__NoBrandAvailable();
+
+contract brand is ERC721URIStorage, Ownable, KeeperCompatibleInterface{
 
     // uint256 public tokenCounter;
     address public creator;
@@ -13,39 +20,47 @@ contract brand is ERC721, Ownable{
     uint256 public totalSupply;
     uint fee;
     uint maxSupply;
+    uint256 private immutable day_interval;
+    uint256 private s_currentTimeStamp;
+    string public brandURI;
+    // bool private isValid;
 
-    // mapping(bytes32 => address) public tokenIdToSender;
-    mapping(uint256 => string) public tokenIdToTokenURI;
+    struct Token{
+        address owner;
+        string tokenURI;
+        uint256 warrantyPeriod;
+        bool isValid;
+    }
 
-    constructor()
+    Token[] private tokens;
+    // mapping(uint256 => address) public tokenIdToOwner;
+    // mapping(uint256 => string) public tokenIdToTokenURI;
+
+    constructor(string memory _brandURI)
     payable
     ERC721("Product", "PRD")
     {
- 
+        brandURI = _brandURI;
         fee = 0.1 * 10 ** 18;
         totalSupply = 0;
-        creator = msg.sender;
         isMintEnabled = true;
         maxSupply = 100;
+        creator = msg.sender;
     }
 
-
-
-
-
-
-
-
-    function createCollectible(string memory tokenURI) 
-        external payable{
+    function createCollectible(string memory _tokenURI, uint256 _warrantyPeriod) 
+        external onlyOwner payable{
         require(isMintEnabled, "Minting is not enabled");
         require(msg.value > fee, "Wrong Value");
         require(maxSupply > totalSupply, "Sold Out");
 
         totalSupply++;
         uint256 tokenId = totalSupply;
-        tokenIdToTokenURI[tokenId] = tokenURI;
-        _safeMint(msg.sender, tokenId);
+        
+        tokens.push(Token(msg.sender, _tokenURI, _warrantyPeriod, false));
+        // warrantyPeriod[tokenId] = _warrantyPeriod;
+        // _safeMint(msg.sender, tokenId);
+        // _setTokenURI(tokenId, tokenURI);
     }
 
     function toggleMint() external onlyOwner {
@@ -56,12 +71,54 @@ contract brand is ERC721, Ownable{
         maxSupply = _maxSupply;
     }
 
+    // function startWarrantyPeriod(uint256 tokenId, uint256 _warrantyPeriod) onlyOwner {
+        
+    // }
 
-    function transferOwnership(address _sender, uint256 _tokenID) public payable{
+
+    function transferOwnership(address _sendTo, uint256 _tokenId) public payable{
         if(msg.sender == creator){
-            _safeMint(_sender, _tokenID);
+            tokens[tokenId].isValid = true;
+        }
+        if(tokens[tokenId].owner == msg.sender){
+            tokens[tokenId].owner = _sendTO;
         }
     }
+
+    function checkUpkeep(
+        bytes memory /* checkData */
+    )
+        public
+        view
+        override
+        returns (
+            bool upkeepNeeded,
+            bytes memory /* performData */
+        )
+    {
+        upkeepNeeded = ((block.timestamp - s_currentTimeStamp) > i_interval);
+        return (upkeepNeeded, "0x0");
+    }
+
+    function performUpkeep(
+        bytes calldata /* performData */
+    ) external override {
+        (bool upkeepNeeded, ) = checkUpkeep("");
+
+        if (!upkeepNeeded) revert Admin__UpkeepNotTrue();
+        if (tokens.length == 0) revert Admin__NoBrandAvailable();
+        if (!isValid) revert NFT_decayed();
+        for (uint256 i = 0; i < tokens.length; i++) {
+            tokens[i].warrantyPeriod -= 1;
+            if (tokens[i].warrantyPeriod == 0) {
+                tokens[i].isValid = false;
+                // delete (tokens[i]);
+            }
+        }
+
+        s_currentTimeStamp = block.timestamp;
+    }
+
     // function setTokenURI(uint256 tokenId, string memory _tokenURI) public {
     //     require(
     //         _isApprovedOrOwner(_msgSender(), tokenId),
@@ -69,4 +126,6 @@ contract brand is ERC721, Ownable{
     //     );
     //     setTokenURI(tokenId, _tokenURI);
     // }
+
+
 }
